c36712b7f5da057b3f70d3351f2657d6646bf7a3
yonnie <yonathanmichael63@gmail.com>
2021-10-20 18:48:31 -0400

    First Commit: determined how to connect to Google API via Node.JS express server

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+
+const { google } = require("googleapis");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.post("/", async (req, res) => {
+	const { request, name } = req.body;
+
+	const auth = new google.auth.GoogleAuth({
+		keyFile: "credentials.json",
+
+		scopes: "https://www.googleapis.com/auth/spreadsheets",
+	});
+
+	// Create Client Instance
+	const client = await auth.getClient();
+
+	// Instance of Google Sheets API
+	const googleSheets = google.sheets({ version: "v4", auth: client });
+
+	const spreadsheetId = "1lF9tJ6LXIEnlVUse5HD_dzqVJGsBaH22fOkUUxLQr6E";
+
+	// Get Metadata about spreadsheet
+	const metadata = await googleSheets.spreadsheets.get({
+		auth,
+
+		spreadsheetId,
+	});
+
+	// Read rows from spreadsheet
+	const getRows = await googleSheets.spreadsheets.values.get({
+		auth,
+
+		spreadsheetId,
+
+		range: "Sheet1",
+	});
+
+	// Write rows to spreadsheet
+
+	await googleSheets.spreadsheets.values.append({
+		auth,
+		spreadsheetId,
+		range: "Sheet1!A:B",
+		valueInputOption: "USER_ENTERED",
+		resource: {
+			values: [[request, name]],
+		},
+	});
+
+	res.send(getRows.data);
+});
+
+app.listen(1337, (req, res) => console.log("running on 1337"));
